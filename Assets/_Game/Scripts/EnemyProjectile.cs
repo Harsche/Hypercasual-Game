7e086aa4d51a9ab3a4d2e6b6f573864befa26e64
Luiz Henrique Harsche <henrique_harsche@hotmail.com>
2021-10-09 23:33:53 -0300

    Projétil inimigo ajeitado

--- Assets/_Game/Scripts/EnemyProjectile.cs
+++ Assets/_Game/Scripts/EnemyProjectile.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour
@@ -23,6 +21,7 @@ public class EnemyProjectile : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             other.GetComponent<Player>().Damage(damage);
+            Destroy(gameObject);
         }
     }
 }
